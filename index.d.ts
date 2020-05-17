5ed747a9865f9790893ef79f7ee4f817e037a718
Pawel <jarrodek@gmail.com>
2020-05-16 18:47:36 -0700

    chore: adding index export to types

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
+export { SelectableMixin } from './src/SelectableMixin';
+export { MultiSelectableMixin } from './src/MultiSelectableMixin';
 export {AnypointSelector} from './AnypointSelector';
 export {AnypointSelection} from './src/AnypointSelection';
