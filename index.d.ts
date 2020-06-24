6db2376a222a5cde72a0959f9733963409c57ece
Pawel <jarrodek@gmail.com>
2020-06-24 11:36:46 -0700

    fix: fixing type export

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export { SelectableMixin } from './src/SelectableMixin';
-export { MultiSelectableMixin } from './src/MultiSelectableMixin';
-export {AnypointSelector} from './AnypointSelector';
-export {AnypointSelection} from './src/AnypointSelection';
+export { SelectableMixin, SelectableMixinConstructor } from './src/SelectableMixin';
+export { MultiSelectableMixin, MultiSelectableMixinConstructor } from './src/MultiSelectableMixin';
+export { AnypointSelector } from './AnypointSelector';
+export { AnypointSelection } from './src/AnypointSelection';
