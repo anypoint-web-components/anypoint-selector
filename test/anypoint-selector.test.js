68c3d08a261152127600af80de48d56d73542e63
Carolina Wright <carolina.wright@mulesoft.com>
2021-08-20 11:54:27 -0300

    test: close event being fired when selecting same element

--- test/anypoint-selector.test.js
+++ test/anypoint-selector.test.js
@@ -1,5 +1,6 @@
 import { fixture, assert, expect, aTimeout } from '@open-wc/testing';
 import '../anypoint-selector.js';
+import * as sinon from 'sinon';
 
 const style = document.createElement('style');
 style.innerHTML = `.selected {
@@ -12,7 +13,7 @@ style.innerHTML = `.selected {
 
 describe('AnypointSelector', () => {
   async function defaultsFixture() {
-    return await fixture(`<anypoint-selector>
+    return fixture(`<anypoint-selector>
       <div>Item 0</div>
       <div>Item 1</div>
       <div>Item 2</div>
@@ -22,7 +23,7 @@ describe('AnypointSelector', () => {
   }
 
   async function basicFixture() {
-    return await fixture(`<anypoint-selector selected="item2" attrforselected="id">
+    return fixture(`<anypoint-selector selected="item2" attrforselected="id">
       <div id="item0">Item 0</div>
       <div id="item1">Item 1</div>
       <div id="item2">Item 2</div>
@@ -31,39 +32,39 @@ describe('AnypointSelector', () => {
     </anypoint-selector>`);
   }
 
-  describe('defaults', function() {
+  describe('defaults', () => {
     let s1;
 
     beforeEach(async () => {
       s1 = await defaultsFixture();
     });
 
-    it('to nothing selected', function() {
+    it('to nothing selected', () => {
       assert.equal(s1.selected, null);
     });
 
-    it('to selected as selectedClass', function() {
+    it('to selected as selectedClass', () => {
       assert.equal(s1.selectedClass, 'selected');
     });
 
-    it('to false as multi', function() {
+    it('to false as multi', () => {
       assert.isFalse(s1.multi);
     });
 
-    it('to click as activateEvent', function() {
+    it('to click as activateEvent', () => {
       assert.equal(s1.activateEvent, 'click');
     });
 
-    it('to nothing as attrForSelected', function() {
+    it('to nothing as attrForSelected', () => {
       assert.equal(s1.attrForSelected, null);
     });
 
-    it('as many items as children', function() {
+    it('as many items as children', () => {
       assert.equal(s1.items.length, s1.querySelectorAll('div').length);
     });
   });
 
-  describe('basic', function() {
+  describe('basic', () => {
     let s2;
 
     beforeEach(async () => {
@@ -77,7 +78,7 @@ describe('AnypointSelector', () => {
       assert.equal(s2.selectedItem, document.querySelector('#item2'));
     });
 
-    it('allows assignment to selected', function() {
+    it('allows assignment to selected', () => {
       // set selected
       s2.selected = 'item4';
       // check selected class
@@ -86,10 +87,11 @@ describe('AnypointSelector', () => {
       assert.equal(s2.selectedItem, s2.children[4]);
     });
 
-    it('fire select when selected is set', function() {
+    it('fire select when selected is set', () => {
       // setup listener for select event
       let selectedEventCounter = 0;
-      s2.addEventListener('select', function() {
+      s2.addEventListener('select', () => {
+        // eslint-disable-next-line no-plusplus
         selectedEventCounter++;
       });
       // set selected
@@ -98,10 +100,11 @@ describe('AnypointSelector', () => {
       assert.equal(selectedEventCounter, 1);
     });
 
-    it('set selected to old value', function() {
+    it('set selected to old value', () => {
       // setup listener for select event
       let selectedEventCounter = 0;
-      s2.addEventListener('select', function() {
+      s2.addEventListener('select', () => {
+        // eslint-disable-next-line no-plusplus
         selectedEventCounter++;
       });
       // selecting the same value shouldn't fire select
@@ -109,8 +112,8 @@ describe('AnypointSelector', () => {
       assert.equal(selectedEventCounter, 0);
     });
 
-    describe('`select()` and `selectIndex()`', function() {
-      it('`select()` selects an item with the given value', function() {
+    describe('`select()` and `selectIndex()`', () => {
+      it('`select()` selects an item with the given value', () => {
         s2.select('item1');
         assert.equal(s2.selected, 'item1');
         s2.select('item3');
@@ -119,7 +122,7 @@ describe('AnypointSelector', () => {
         assert.equal(s2.selected, 'item2');
       });
 
-      it('`selectIndex()` selects an item with the given index', function() {
+      it('`selectIndex()` selects an item with the given index', () => {
         // This selector has attributes `selected` and `attr-for-selected`
         // matching this item.
         assert.equal(s2.selectedItem, s2.items[2]);
@@ -135,18 +138,19 @@ describe('AnypointSelector', () => {
       });
     });
 
-    describe('items changing', function() {
+    describe('items changing', () => {
       let s1;
       beforeEach(async () => {
         s1 = await defaultsFixture();
       });
 
       it('cause children-changed to fire', async () => {
-        let newItem = document.createElement('div');
+        const newItem = document.createElement('div');
         let changeCount = 0;
         newItem.id = 'item999';
 
-        s2.addEventListener('children-changed', function(event) {
+        s2.addEventListener('children-changed', (event) => {
+          // eslint-disable-next-line no-plusplus
           changeCount++;
           const mutations = event.detail;
           assert.typeOf(mutations, 'array');
@@ -161,13 +165,13 @@ describe('AnypointSelector', () => {
         expect(changeCount).to.be.equal(2);
       });
 
-      it('updates selected item', function(done) {
+      it('updates selected item', (done) => {
         s1.selected = 0;
-        let firstElementChild = s1.firstElementChild;
+        let {firstElementChild} = s1;
         expect(firstElementChild).to.be.equal(s1.selectedItem);
         expect(firstElementChild.classList.contains('selected')).to.be.eql(true);
 
-        s1.addEventListener('children-changed', function() {
+        s1.addEventListener('children-changed', () => {
           firstElementChild = s1.firstElementChild;
           expect(firstElementChild).to.be.equal(s1.selectedItem);
           expect(firstElementChild.classList.contains('selected')).to.be.eql(true);
@@ -175,19 +179,36 @@ describe('AnypointSelector', () => {
         });
         s1.removeChild(s1.selectedItem);
       });
+
+      it('navigation-close dispatched when selecting same item', () => {
+        const spy = sinon.spy();
+        s1.addEventListener('closed', spy);
+
+        s1.selected = 0;
+        let { firstElementChild } = s1;
+        expect(firstElementChild).to.be.equal(s1.selectedItem);
+        expect(firstElementChild.classList.contains('selected')).to.be.eql(true);
+        assert.isFalse(spy.called);
+
+        s1.selected = 0;
+        firstElementChild = s1.firstElementChild;
+        expect(firstElementChild).to.be.equal(s1.selectedItem);
+        expect(firstElementChild.classList.contains('selected')).to.be.eql(true);
+        assert.isTrue(spy.called);
+      });
     });
 
-    describe('dynamic selector', function() {
+    describe('dynamic selector', () => {
       // NOTE(bicknellr): Polymer 2 only upgrades elements once they have been
       // connected to the document. This test now connects the selector
       // *first* and appends the child afterwards.
-      it('selects dynamically added child automatically', function(done) {
+      it('selects dynamically added child automatically', (done) => {
         // Create the selector, set selected, connect to force upgrade.
-        let selector = document.createElement('anypoint-selector');
+        const selector = document.createElement('anypoint-selector');
         selector.selected = '0';
         document.body.appendChild(selector);
         // Create and append a new item which should become selected.
-        let child = document.createElement('div');
+        const child = document.createElement('div');
         child.textContent = 'Item 0';
         selector.appendChild(child);
         selector.addEventListener('children-changed', function onIronItemsChanged() {
