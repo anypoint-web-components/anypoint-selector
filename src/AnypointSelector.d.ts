bd14974854020d202c5e58f65bd70805776ab526
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2021-08-27 10:52:47 -0300

    Merging stage into master (#16)
    * chore: updating types
    
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>
    
    * chore: updating dependencies
    
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>
    
    * fix: dispatch close event when choosing selected element
    
    * build: bump version
    
    * chore: ignore .idea folder
    
    * chore: add deployment.yml
    
    * test: close event being fired when selecting same element
    
    Co-authored-by: Ci agent <arc@mulesoft.com>
    Co-authored-by: Pawel Psztyc <jarrodek@gmail.com>
    Co-authored-by: Carolina Wright <carolina.wright@mulesoft.com>

--- src/AnypointSelector.d.ts
+++ src/AnypointSelector.d.ts
@@ -1,11 +1,9 @@
 import {LitElement} from 'lit-element';
 import {MultiSelectableMixin} from './MultiSelectableMixin';
 
-export declare class AnypointSelector {
+export declare class AnypointSelector extends MultiSelectableMixin(LitElement) {
   createRenderRoot(): AnypointSelector;
-}
-export interface AnypointSelector extends MultiSelectableMixin, LitElement {
- /**
+  /**
    * @returns Previously registered handler for `select` event
    */
   onselect: EventListener;
